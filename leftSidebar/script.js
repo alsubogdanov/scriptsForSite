75ee178a31bf31573773cfb672347522d3e6b209
Alsu Bogdanova <47866010+alsousha@users.noreply.github.com>
2021-07-25 11:15:45 +0300

    Add files via upload

---
+++ leftSidebar/script.js
@@ -0,0 +1,16 @@
+const sidebar = document.querySelector(".sidebar");
+const closeBtn = document.querySelector(".icon-close")
+const openBtn = document.querySelector(".icon-open");
+
+closeBtn.addEventListener("click", function (){
+    sidebar.classList.add("sidebar--closed");
+    sidebar.classList.remove("sidebar--opened");
+    closeBtn.style.display="none"
+    openBtn.style.display="block"
+});
+openBtn.addEventListener("click", function (){
+    sidebar.classList.add("sidebar--opened")
+    sidebar.classList.remove("sidebar--closed")
+    openBtn.style.display="none"
+    closeBtn.style.display="block"
+});
